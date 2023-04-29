23de7974698e3049f0bd70f8615c91d53bf955b0
Pedro Caseiro <>
2023-04-29 22:50:25 +0100

    window location reload

--- next_app/components/new_poll.js
+++ next_app/components/new_poll.js
@@ -38,7 +38,7 @@ export default function PageWithJSbasedForm() {
 
         // Get the response data from server as JSON.
         // If server returns the name submitted, that means the form works.
-        location.reload()
+        window.location.reload()
     }
     return (
         // We pass the event to the handleSubmit() function on submit.
