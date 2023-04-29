2e1a876dede187d4eb56a905f25e21f308a326d7
Pedro Caseiro <>
2023-04-29 21:59:01 +0100

    fix votes

--- next_app/pages/api/vote.js
+++ next_app/pages/api/vote.js
@@ -7,7 +7,13 @@ export default function handler(req, res) {
         case "POST":
             const {id, answer} = req.body;
             let answer_id = "answer" + answer;
-            polls[id-1][answer_id].votes += 1;
+            // find pool by id
+            for (let i = 0; i < polls.length; i++) {
+                if (polls[i].id === id) {
+                    polls[i][answer_id].votes += 1;
+                    break;
+                }
+            }
             res.status(200).json(polls);
             break;
         default:
